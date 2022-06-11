d6eababaa9ea2161344264f35ac51c9f45452ce1
pikemd <101365965+pikemd@users.noreply.github.com>
2022-06-11 12:45:44 -0500

    files
    files

---
+++ Pewlett-Hackard-Analysis/Queries/Employee_Database_challenge.sql
@@ -0,0 +1,34 @@
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+
+INTO unique_titles
+FROM retirement_titles 
+WHERE (to_date = '9999-01-01')
+ORDER BY emp_no, to_date DESC;
+
+--Additional query retrive the number of employees by most recent job title, about to retire
+SELECT COUNT(title), title FROM unique_titles
+GROUP BY title
+
+--Mentorship Eligibility
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+ti.title 
+
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+
+WHERE (de.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no DESC;
